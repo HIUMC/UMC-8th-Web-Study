a02175f54623b13441b6e4da2d7f179cf7485fca
shinwokkang <tlsdnr303@gmail.com>
2025-03-24 11:38:27 +0900

    Refactor: 강의 내용과 동일하게 코드 변경

--- heungboo/Week_02/src/App.tsx
+++ heungboo/Week_02/src/App.tsx
@@ -1,53 +1,9 @@
+import { ReactElement } from "react";
 import "./App.css";
-import { useState } from "react";
-import Header from "./components/Header";
-import Render from "./components/Render";
+import Todo from "./components/Todo";
 
-function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState<string[]>([]);
-  const [doneTodos, setDoneTodos] = useState<string[]>([]);
-  const [deleteTodo, setDeleteTodo] = useState<string[]>([]);
-
-  const handleChange = (e) => {
-    // console.log("e.target.value : " + e.target.value);
-    setInputValue(e.target.value);
-  };
-
-  const handleAddTodo = () => {
-    if (inputValue.trim() === "") return;
-
-    setTodos([...todos, inputValue]);
-    setInputValue("");
-  };
-
-  const handleCompleteTodo = (index: number) => {
-    const completedTodo = todos[index];
-    setDoneTodos([...doneTodos, completedTodo]);
-    setTodos(todos.filter((_, i) => i !== index));
-  };
-
-  const handleDeleteTodo = (index: number) => {
-    const delTodo = doneTodos[index];
-    setDeleteTodo([...deleteTodo, delTodo]);
-    setDoneTodos(doneTodos.filter((_, i) => i !== index));
-  };
-
-  return (
-    <div className="todo-container">
-      <Header
-        inputValue={inputValue}
-        handleChange={handleChange}
-        handleAddTodo={handleAddTodo}
-      />
-      <Render
-        todos={todos}
-        handleCompleteTodo={handleCompleteTodo}
-        doneTodos={doneTodos}
-        handleDeleteTodo={handleDeleteTodo}
-      />
-    </div>
-  );
+function App(): ReactElement {
+  return <Todo />;
 }
 
 export default App;
