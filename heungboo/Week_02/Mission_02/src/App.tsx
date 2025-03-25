275236256096acaa3886d937c09033c6acf2ddd8
shinwokkang <tlsdnr303@gmail.com>
2025-03-25 17:26:53 +0900

    feat: Theme Toggle useContext 기능 개발

--- heungboo/Week_02/Mission_02/src/App.tsx
+++ heungboo/Week_02/Mission_02/src/App.tsx
@@ -2,13 +2,15 @@ import { ReactElement } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
 import { TodoProvider } from "./context/todoContext";
-
+import { ThemeProvider } from "./context/ThemeProvider";
 function App(): ReactElement {
   return (
-    <TodoProvider>
-      <Todo />
-      {/* <TodoBefore /> */}
-    </TodoProvider>
+    <ThemeProvider>
+      <TodoProvider>
+        <Todo />
+        {/* <TodoBefore /> */}
+      </TodoProvider>
+    </ThemeProvider>
   );
 }
 
--- heungboo/Week_02/Mission_02/src/components/Todo.tsx
+++ heungboo/Week_02/Mission_02/src/components/Todo.tsx
@@ -1,14 +1,19 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import { useTodo } from "../context/todoContext";
+import { ThemeContext } from "@emotion/react";
+import ThemeToggleButton from "./ThemeToggleButton";
 
 const Todo = (): ReactElement => {
   const { todos, doneTodos, completeTodo, deleteTodo } = useTodo();
+  const context = useContext(ThemeContext);
+  console.log(context);
 
   return (
     <div className="todo-container">
       <h1 className="todo-container__header">YONG TODO</h1>
+      <ThemeToggleButton />
       <TodoForm />
       <div className="render-container">
         <TodoList
