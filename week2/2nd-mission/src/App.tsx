d5c3ec7eb39c3b83c2b9987ccb4f52d45b3083e5
cchaeyoon <codbs0722@gmail.com>
2025-03-27 02:25:36 +0900

    week2 미션1

---
+++ week2/2nd-mission/src/App.tsx
@@ -0,0 +1,14 @@
+import { JSX } from 'react';
+import './App.css'
+import { TodoProvider } from './context/TodoContext';
+import Todo from './components/todo';
+
+function App() : JSX.Element {
+  return (
+    <TodoProvider>
+      <Todo />
+    </TodoProvider>
+  );
+}
+
+export default App;
