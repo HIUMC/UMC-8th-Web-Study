a2c413bb25bec2322658f894c01aa997011394d2
cchaeyoon <codbs0722@gmail.com>
2025-03-27 18:06:46 +0900

    week2

---
+++ week2/mission2/src/useContext/Contextpage.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "react";
+import Navbar from "./Navbar";
+import ThemeContent from "./ThemeContent";
+
+export default function Contextpage(): JSX.Element {
+  return(
+    <>
+        <Navbar />
+        <ThemeContent />
+    </>
+  )
+}
---
+++ week2/mission2/src/useContext/ThemeContent.tsx
@@ -0,0 +1,5 @@
+import { JSX } from "react";
+
+export default function ThemeContent(): JSX.Element {
+  return <div>ThemeContent</div>;
+}
