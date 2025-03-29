de6e80e79ea3d2584d1bf702e9795a2730f4d39a
jinj00oo <icems0428@naver.com>
2025-03-30 00:05:05 +0900

    Week1 & Week2 미션 업로드

---
+++ week2/mission2/src/components/ThemeContent.tsx
@@ -0,0 +1,30 @@
+import { THEME, useTheme } from "../context/ThemeProvider";
+import clsx from "clsx";
+
+export default function ThemeContent(): JSX.Element {
+  const { theme, toggleTheme } = useTheme();
+
+  const isLightMode = theme === THEME.LIGHT;
+  return (
+    <div
+      className={clsx(
+        "p-4 h-dvh w-full",
+        isLightMode ? "bh-white" : "bg-gray-800"
+      )}
+    >
+      <h1
+        className={clsx(
+          "text-wxl font-bold",
+          isLightMode ? "text-black" : "text-white"
+        )}
+      >
+        Theme Content
+      </h1>
+      <p className={clsx("mt-2", isLightMode ? "text-black" : "text-white")}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum aliquam
+        deleniti magnam nobis, sequi dolorum error at ullam ipsam autem rem
+        vitae. Rerum, repellat itaque animi architecto repellendus facilis ex.
+      </p>
+    </div>
+  );
+}
