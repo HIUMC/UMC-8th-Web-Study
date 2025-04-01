3971db8d1829f3fa35d9a61cea76a9c37e5d4925
seokhoon <excava.hoon76@g.hongik.ac.kr>
2025-04-01 22:19:59 +0900

    fix confilcts

--- ily/chapter03mission02/src/pages/HomePage.tsx
+++ ily/chapter03mission02/src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import { Navbar } from "../components/Navbar";
 
 export default function HomePage() {
   //children을 설정했으므로 children을 기준으로outlet을 설정해줘야 parents 기준의 children이 제대로 렌더링이 됨.
-  //그냥 주석 추가함
   return (
     <>
       <Navbar />
