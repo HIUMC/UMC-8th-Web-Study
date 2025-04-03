a100b23d4c5464039175c3cc27220f91e7c8f6fb
박세현 <sehyun@psh.local>
2025-04-03 16:45:22 +0900

    Replace week2 folder with new version

---
+++ week3/mission1/movie/src/App.tsx
@@ -0,0 +1,12 @@
+import { ReactElement } from 'react';
+import './App.css'
+import MoviePage from './pages/MoviePage';
+
+function App(): ReactElement{
+  return (
+  <>
+  <MoviePage/>
+  </>
+  );
+}
+export default App;
