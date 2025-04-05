6d7f1c70a038acf13ddcf4b1a0ba1260fe679c2e
cchaeyoon <codbs0722@gmail.com>
2025-04-05 22:36:41 +0900

    fix

---
+++ pochae/week2/mission2/src/App.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+import './App.css';
+import Contextpage from './useContext/Contextpage';
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <Contextpage />
+    </>
+  );
+}
