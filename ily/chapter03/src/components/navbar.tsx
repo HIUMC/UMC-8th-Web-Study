0de4803a712f160218b5d5e0aa6c24aea49fd88e
seokhoon <excava.hoon76@g.hongik.ac.kr>
2025-04-07 14:56:34 +0900

    rebase for merge branches histories

--- ily/chapter03/src/components/navbar.tsx
+++ ily/chapter03/src/components/navbar.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
+    <div>
       <nav>
         <Link to={"/"}>홈페이지로 이동</Link>
         <Link to={"/movies"}>영화 페이지로 이동</Link>
       </nav>
-    </>
+    </div>
   );
 };
 
