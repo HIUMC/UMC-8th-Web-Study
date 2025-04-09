9291a049f77c4871cecd66b05318dd6fde0d09cc
oohrii <jodie415@naver.com>
2025-04-10 01:04:30 +0900

    회원가입 페이지 생성

--- oohrii/umc-8th-mission-FE/src/App.tsx
+++ oohrii/umc-8th-mission-FE/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </Router>
   );
