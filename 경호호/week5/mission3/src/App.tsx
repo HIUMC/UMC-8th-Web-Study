e52c2a79edbdad4e9e3281599c25022321168cb1
Kyoungho Eom <kheom0512@gmail.com>
2025-05-02 03:53:52 +0900

    feat: 사용자 인증 기능 구현

---
+++ 경호호/week5/mission3/src/App.tsx
@@ -0,0 +1,37 @@
+import { Routes, Route } from 'react-router-dom';
+import SignupPage from './pages/SignupPage';
+import HomePage from './pages/HomePage';
+import SigninPage from './pages/SigninPage';
+import OAuthRedirectHandler from './pages/OAuthRedirectHandler';
+import UsersMePage from './pages/UsersMePage';
+import UserDetailPage from './pages/UserDetailPage';
+import UserDeletePage from './pages/UserDeletePage';
+import UserEditPage from './pages/UserEditPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import { AuthProvider } from './contexts/AuthContext';
+
+function App() {
+  return (
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900 text-white">
+        <main className="flex-grow flex items-center justify-center p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/signin" element={<SigninPage />} />
+            <Route path="/oauth2/redirect" element={<OAuthRedirectHandler />} />
+            
+            <Route element={<ProtectedRoute />}>
+              <Route path="/users/me" element={<UsersMePage />} />
+              <Route path="/users/:userId" element={<UserDetailPage />} />
+              <Route path="/users/delete" element={<UserDeletePage />} />
+              <Route path="/users/edit" element={<UserEditPage />} />
+            </Route>
+          </Routes>
+        </main>
+      </div>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ 경호호/week5/mission3/src/pages/OAuthRedirectHandler.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const OAuthRedirectHandler = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { socialLogin } = useAuth();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const accessToken = params.get('accessToken');
+    const refreshToken = params.get('refreshToken');
+    if (accessToken && refreshToken) {
+      socialLogin({ accessToken, refreshToken });
+      navigate('/users/me', { replace: true });
+    } else {
+      navigate('/signin', { replace: true });
+    }
+  }, [location.search, socialLogin, navigate]);
+
+  return <div className="text-center">로그인 처리중...</div>;
+};
+
+export default OAuthRedirectHandler; 
