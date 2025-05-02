06c6eebf8971b4afe9357afe11310c0a96f46b39
HiJuwon <wizz2176ytr@naver.com>
2025-05-03 07:13:35 +0900

    week5 mission 1, 2, 3

---
+++ Juju/week5/week5-mission2/src/pages/LoginPage.tsx
@@ -0,0 +1,81 @@
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext";
+import useForm from "../hooks/useForm";
+import { UserSigninInformation, validateSignin } from "../utils/validate";
+import { useNavigate } from "react-router-dom";
+
+const LoginPage = () => {
+    const { login, accessToken } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (accessToken) {
+            navigate("/")
+        }
+    }, [accessToken, navigate]);
+
+    const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
+        initialValue: {
+            email: "",
+            password: "",
+        },
+        validate: validateSignin,
+    });
+
+    const handleSubmit = async () => {
+        try {
+            await login(values);
+            navigate("/my"); // 로그인 성공 시 마이페이지로 이동
+        } catch {
+            alert("로그인에 실패했습니다."); // 로그인 실패 시 알림
+        }
+    };
+
+
+    // 오류가 하나라도 있거나, 입력값이 비어있으면 버튼을 비활성화
+    const isDisabled = Object.values(errors || {}).some((error : string) => error.length > 0) || Object.values(values).some((value) => value === "");
+  
+  return <div className="flex flex-col items-center justify-center h-full gap-4">
+    <button
+        onClick={() => navigate(-1)}
+        className="absolute top-4 left-4 text-lg text-black hover:text-gray-500 
+             cursor-pointer px-4 py-2 rounded-md transition-all"
+    >
+        뒤로가기
+    </button>
+    <div className="flex flex-col gap-3">
+        <input
+            {...getInputProps("email")}
+            name="email"
+            className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+                ${errors?.email && touched?.email ? "border-red-500" : "border-gray-300"}`}
+            type={"email"}
+            placeholder={"이메일"}
+        />
+        {errors?.email && touched?.email && (
+            <div className="text-red-500 text-sm">{errors.email}</div>
+        )}
+        <input
+            {...getInputProps("password")}
+            className={`border border-[#ccc] w-[300px] p-[10px] focus:border-[#807bff] rounded-sm
+                ${errors?.password && touched?.password ? "border-red-500" : "border-gray-300"}`}
+            type={"password"}
+            placeholder={"패스워드"}
+        />
+        {errors?.password && touched?.password && (
+            <div className="text-red-500 text-sm">{errors.password}</div>
+        )}
+        <button
+            type="button"
+            onClick={handleSubmit}
+            disabled={isDisabled}
+            className="w-full bg-black text-white py-3 rounded-md text-lg font-medium hover:bg-gray-800 transition-colors duration-200 cursor-pointer disabled:bg-gray-300 disabled:bg-gray-300"
+        >
+            로그인
+        </button>
+        
+    </div>
+  </div>
+}
+
+export default LoginPage
---
+++ Juju/week5/week5-mission2/src/pages/MyPage.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { getMyInfo } from "../apis/auth";
+import { ResponseMyInfoDto } from "../types/auth";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const MyPage = () => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+    const [data, setData] = useState<ResponseMyInfoDto | null>(null);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await getMyInfo();
+            console.log(response);
+
+            setData(response);
+        };
+
+        getData();
+    }, []);
+
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");  // 로그아웃 후 홈으로 이동
+  };
+  
+  console.log(data?.data?.name);
+
+  return (
+    <div>
+      <h1>{data?.data.name}님 환영합니다!</h1>
+      <img src={data?.data?.avatar as string} alt={"구글 로고"} />
+      <h1>{data?.data?.email}</h1>
+
+      <button
+        className="cursor-pointer bg-black text-white rounded-sm p-5 hover:bg-gray-700"
+        onClick={handleLogout}
+      >
+        로그아웃
+      </button>
+    </div>
+  )
+};
+
+export default MyPage;
