a37b79850d4f47fecc042322054bbbdaf33e3d3d
seokhoon <excava.hoon76@g.hongik.ac.kr>
2025-05-08 14:23:28 +0900

    fix folder structure

--- ily/chapter06/src/components/LpCard/LpCard.tsx
+++ ily/chapter06/src/components/LpCard/LpCard.tsx
@@ -1,22 +1,32 @@
-import { useParams, Link } from "react-router-dom";
 import { LP } from "../../utils/types/lp";
+import { useEffect } from "react";
+import { axiosInstance } from "../../apis/axios";
+import { useNavigate } from "react-router-dom";
 
 interface LpCardProps {
   lp: LP;
 }
 
 const LpCard = ({ lp }: LpCardProps) => {
-  const { LPid } = useParams();
+  const navigate = useNavigate();
+
+  const handler = () => {
+    console.log("onclick작동ㅓ");
+    console.log("LP", lp.id);
+
+    navigate(`/lps/${lp.id}`);
+  };
   return (
     <>
-      <div className="relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-500 group hover:scale-105 cursor-pointer">
-        <Link to={`/lp/${LPid}`}>
-          <img
-            src={lp.thumbnail}
-            alt={lp.title}
-            className="object-cover w-full h-48"
-          />
-        </Link>
+      <div
+        onClick={handler}
+        className="relative rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-500 group hover:scale-105 cursor-pointer"
+      >
+        <img
+          src={lp.thumbnail}
+          alt={lp.title}
+          className="object-cover w-full h-48"
+        />
         {/* Hover 시 나타나는 반투명 오버레이 */}
         <div className="absolute inset-0 bg-black bg-opacity-75 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center ">
           <p className="text-white text-lg font-semibold mb-2">{lp.title}</p>
