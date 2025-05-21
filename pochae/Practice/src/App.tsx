d20fe66e0b1946be5a6711d60b01c2db8038ab45
cchaeyoon <codbs0722@gmail.com>
2025-05-21 22:33:18 +0900

    week9 useReducer

---
+++ pochae/Practice/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import UseReducerCompany from './useReducer/UseReducerCompany';
+// import UseReducerPage from './useReducer/UseReducerPage'
+
+export default function App() {
+  return (
+    <>
+      <UseReducerCompany />
+    </>
+  );
+}
