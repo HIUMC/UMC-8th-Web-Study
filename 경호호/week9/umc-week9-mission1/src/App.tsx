086f7d6b5c3c31dba0ce85bd387c581bd086c36b
Kyoungho Eom <kheom0512@gmail.com>
2025-05-29 21:31:19 +0900

    feat: 장바구니 Redux 상태관리 및 UI 구현

--- 경호호/week9/umc-week9-mission1/src/App.tsx
+++ 경호호/week9/umc-week9-mission1/src/App.tsx
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar';
+import CartList from './components/CartList';
+
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-white shadow-lg">
-        Tailwind CSS 설치 완료! 🎉
-      </h1>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <CartList />
     </div>
   );
 }
---
+++ 경호호/week9/umc-week9-mission1/src/constants/albumItems.ts
@@ -0,0 +1,44 @@
+import { LP } from '../types';
+
+export const albumItems: LP[] = [
+  {
+    id: '1',
+    title: 'IU 5th Album LILAC',
+    singer: 'IU',
+    price: 25000,
+    img: 'https://i.ibb.co/XDfKrFx/lilac.jpg',
+    amount: 1,
+  },
+  {
+    id: '2',
+    title: 'LOVE YOURSELF: TEAR',
+    singer: 'BTS',
+    price: 28000,
+    img: 'https://i.ibb.co/h1sJT7C/tear.jpg',
+    amount: 1,
+  },
+  {
+    id: '3',
+    title: 'An Ode',
+    singer: 'SEVENTEEN',
+    price: 23000,
+    img: 'https://i.ibb.co/vsXDPRJ/ode.jpg',
+    amount: 1,
+  },
+  {
+    id: '4',
+    title: 'MAP OF THE SOUL: 7',
+    singer: 'BTS',
+    price: 30000,
+    img: 'https://i.ibb.co/DW9TH3y/soul.jpg',
+    amount: 1,
+  },
+  {
+    id: '5',
+    title: 'Love Poem',
+    singer: 'IU',
+    price: 24000,
+    img: 'https://i.ibb.co/0K8Zkgb/poem.jpg',
+    amount: 1,
+  },
+];
