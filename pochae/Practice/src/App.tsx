e03facfb99834cda060b8251c3117273b2d0d698
cchaeyoon <codbs0722@gmail.com>
2025-06-19 17:41:06 +0900

    week10

--- pochae/Practice/src/App.tsx
+++ pochae/Practice/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import UseCallbackPage from './useCallback-memo/useCallbackPage';
 // import UseReducerPage from './useReducer/UseReducerPage'
 
 export default function App() {
   return (
-    <>
+    <main className='flex flex-col justify-center items-center h-dvh'>
       <UseCallbackPage/>
-    </>
+    </main>
   );
 }
